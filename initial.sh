c58e1848f0980aa3d61556c2320f11e5ed7bae86
nouha zouaghi <60858818+nouha57@users.noreply.github.com>
2023-03-14 11:08:14 +0000

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -49,6 +49,10 @@ unzip awscliv2.zip
 sudo ./aws/install
 
 
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv /tmp/eksctl /usr/local/bin
+
 sudo yum -y install jq gettext bash-completion moreutils
 
 
